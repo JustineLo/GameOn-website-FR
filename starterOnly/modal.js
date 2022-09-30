568697f715fa798d15a53194ced9b6bae05f96bb
Justine Lo <justine@ecotree.fr>
2022-09-30 23:04:04 +0200

    add form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,13 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit")
+const firstInput = document.getElementById("first");
+const lastInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const quantityInput = document.getElementById("quantity");
+const cities = document.querySelectorAll("input[type=radio]");
+const termsCheckbox = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,3 +36,53 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// launch submit button event
+submitBtn.addEventListener("click", function (event) {
+  if (isValidText(firstInput) && isValidText(lastInput) && isValidQuantity(quantityInput) && isCityChecked() && isTermsChecked() && isValidEmail()) {
+
+
+    console.log("Submitted")
+  } else {
+    // prevent page from reloading
+    event.preventDefault();
+  }
+});
+
+// test if text fields are empty or less than 2 letters
+function isValidText(inputValue) {
+  return inputValue != null && inputValue.value.length >= 2
+}
+
+// test if email is valid
+function isValidEmail() {
+  return emailInput != null && emailInput.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
+}
+
+// test is quantity is number
+function isValidQuantity(inputValue) {
+  return inputValue && inputValue.value.match(/^\d{1,}$/)
+}
+
+// test if a city checkbox is checked
+function isCityChecked() {
+  for (let i = 0; i < cities.length; i++) {
+    if (cities[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// test if terms checkbox is checked 
+function isTermsChecked() {
+  if (termsCheckbox.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// send form
+function validate(event) {
+
+}
